b4081059f621858b87cf8ff7f2d5d783d85d6aaa
49963270@qq.com <49963270@qq.com>
2018-07-20 08:50:59 +0800

    增加图形验证码功能

--- Web/Controllers/LoginController.cs
+++ Web/Controllers/LoginController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Interface;
+using Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,51 @@ namespace Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        private IAuth _authUtil;
+
+        public LoginController(IAuth authUtil)
+        {
+            _authUtil = authUtil;
+        }
+
+        /// <summary>
+        /// 数字验证码
+        /// </summary>
+        /// <returns></returns>
+        public FileContentResult NumberVerifyCode()
+        {
+            string code = VerifyCodeHelper.GetSingleObj().CreateVerifyCode(VerifyCodeHelper.VerifyCodeType.NumberVerifyCode);
+            byte[] codeImage = VerifyCodeHelper.GetSingleObj().CreateByteByImgVerifyCode(code, 100, 40);
+            return File(codeImage, @"image/jpeg");
+        }
+
+
+        public string Login(string password, string username)
+        {
+            var resp = new Response();
+            try
+            {
+                var result = _authUtil.Login("", username, password);
+                if (result.Code == 200)
+                {
+                    Response.Cookies.Append("Token", result.Token);
+                }
+                else
+                {
+                    resp.Code = 500;
+                    resp.Message = result.Message;
+                }
+
+            }
+            catch (Exception e)
+            {
+                resp.Code = 500;
+                resp.Message = e.Message;
+            }
+
+            return JsonHelper.Instance.Serialize(resp);
         }
     }
 }
